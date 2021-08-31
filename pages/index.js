0ee29e498c4a82acd00ce1b55eed8ebde17c3dd4
Klaudiusz-Marszalek <klaudiusz.w.m@gmail.com>
2021-08-31 22:15:54 +0200

    handle viewport problem

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
           <title>React Conditional Selection</title>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=0.8, maximum-scale=5.0, minimum-scale=0.8"
+            content="width=device-width, initial-scale=0.8, maximum-scale=1.0, minimum-scale=0.8"
           ></meta>
           <meta
             name="description"
